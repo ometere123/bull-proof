5a48b942a1c69c803a808094d868437e4930e982
papitodele <45469370+ometere123@users.noreply.github.com>
2026-08-23 21:34:30 +0100

    test: enable direct-mode storage serialization checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def _reset_contract_registry():
+def _configure_direct_mode(direct_vm):
+    """Catch storage-serialization mistakes and isolate contract definitions per test."""
+    direct_vm.check_pickling = True
     yield
     try:
         import genlayer.gl.genvm_contracts as contracts
